20262ad76708d05a93a02ca5d86ebb8f101e607f
Josh Junon <junon@oro.sh>
2023-07-23 18:43:27 +0200

    update chip packages to latest version

--- Firmware/src/oro-link-stm32f7/src/main.rs
+++ Firmware/src/oro-link-stm32f7/src/main.rs
@@ -1,26 +1,23 @@
 #![no_main]
 #![no_std]
-extern crate cortex_m;
-extern crate cortex_m_rt as runtime;
-extern crate stm32f7;
-
 use core::panic::PanicInfo;
-use cortex_m::asm;
 use stm32f7::stm32f7x6::Peripherals;
 
 #[no_mangle]
 fn main() -> ! {
 	let per = Peripherals::take().unwrap();
 	per.RCC.ahb1enr.write(|w| w.gpioben().bit(true));
-	per.GPIOB.moder.write(|w| w.moder7().bits(0b01));
+	per.GPIOB.moder.write(|w| w.moder7().output());
 
 	let mut on: bool = true;
 	loop {
 		per.GPIOB.odr.modify(|_, w| w.odr7().bit(on));
 		on = !on;
 
-		for _i in 0..50000 {
-			asm::nop()
+		for _ in 0..1000000 {
+			unsafe {
+				core::arch::asm!("NOP");
+			}
 		}
 	}
 }
