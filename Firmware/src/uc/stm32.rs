dd141756cd841054557ffd9f3198232f010277e6
Josh Junon <junon@oro.sh>
2023-08-04 22:46:09 +0200

    move STM32F4 types to common STM32 crate

--- Firmware/src/uc/stm32.rs
+++ Firmware/src/uc/stm32.rs
@@ -3,8 +3,11 @@ mod stm32f479vg;
 #[cfg(feature = "stm32f479vg")]
 pub use stm32f479vg::*;
 
-use crate::{chip, uc::Color};
-use embassy_stm32::i2c;
+use crate::chip;
+use embassy_stm32::{
+	gpio::{Input, Output, Pin},
+	i2c,
+};
 
 impl<'d, T: i2c::Instance, TXDMA, RXDMA> chip::I2c for i2c::I2c<'d, T, TXDMA, RXDMA> {
 	type Error = i2c::Error;
@@ -67,10 +70,9 @@ impl<
 	const R2: u8,
 	const G2: u8,
 	const B2: u8,
-> crate::uc::IndicatorLights
-	for Is31fl3218IndicatorLights<I2C, R0, G0, B0, R1, G1, B1, R2, G2, B2>
+> super::IndicatorLights for Is31fl3218IndicatorLights<I2C, R0, G0, B0, R1, G1, B1, R2, G2, B2>
 {
-	fn first<C: Into<Color>>(&mut self, color: C) {
+	fn first<C: Into<super::Color>>(&mut self, color: C) {
 		let (r, g, b) = color.into().premultiply_alpha();
 		self.0.set_channel(R0, r);
 		self.0.set_channel(G0, g);
@@ -78,7 +80,7 @@ impl<
 		self.0.present();
 	}
 
-	fn second<C: Into<Color>>(&mut self, color: C) {
+	fn second<C: Into<super::Color>>(&mut self, color: C) {
 		let (r, g, b) = color.into().premultiply_alpha();
 		self.0.set_channel(R1, r);
 		self.0.set_channel(G1, g);
@@ -86,7 +88,7 @@ impl<
 		self.0.present();
 	}
 
-	fn third<C: Into<Color>>(&mut self, color: C) {
+	fn third<C: Into<super::Color>>(&mut self, color: C) {
 		let (r, g, b) = color.into().premultiply_alpha();
 		self.0.set_channel(R2, r);
 		self.0.set_channel(G2, g);
@@ -104,5 +106,120 @@ impl<
 
 	fn enable(&mut self) {
 		self.0.enable();
+	}
+}
+
+pub struct DebugLed<'d, P: Pin> {
+	pin: Output<'d, P>,
+}
+
+impl<'d, P: Pin> DebugLed<'d, P> {
+	pub fn new(pin: Output<'d, P>) -> Self {
+		Self { pin }
+	}
+}
+
+impl<P: Pin> super::DebugLed for DebugLed<'_, P> {
+	fn set_bit(&mut self, on: bool) {
+		self.pin.set_level(on.into());
+	}
+}
+
+pub struct SystemUnderTest<'d, RST, PWR, PSUON, PSUSB, SYSON>
+where
+	RST: Pin,
+	PWR: Pin,
+	PSUON: Pin,
+	PSUSB: Pin,
+	SYSON: Pin,
+{
+	current_state: super::PowerState,
+	reset_pin: Output<'d, RST>,
+	power_pin: Output<'d, PWR>,
+	psu_on_pin: Output<'d, PSUON>,
+	psu_standby_pin: Output<'d, PSUSB>,
+	sys_on_pin: Input<'d, SYSON>,
+}
+
+impl<'d, RST, PWR, PSUON, PSUSB, SYSON> SystemUnderTest<'d, RST, PWR, PSUON, PSUSB, SYSON>
+where
+	RST: Pin,
+	PWR: Pin,
+	PSUON: Pin,
+	PSUSB: Pin,
+	SYSON: Pin,
+{
+	pub fn new(
+		reset_pin: Output<'d, RST>,
+		power_pin: Output<'d, PWR>,
+		psu_on_pin: Output<'d, PSUON>,
+		psu_standby_pin: Output<'d, PSUSB>,
+		sys_on_pin: Input<'d, SYSON>,
+	) -> Self {
+		Self {
+			current_state: super::PowerState::Off,
+			reset_pin,
+			power_pin,
+			psu_on_pin,
+			psu_standby_pin,
+			sys_on_pin,
+		}
+	}
+}
+
+impl<'d, RST, PWR, PSUON, PSUSB, SYSON> super::SystemUnderTest
+	for SystemUnderTest<'d, RST, PWR, PSUON, PSUSB, SYSON>
+where
+	RST: Pin,
+	PWR: Pin,
+	PSUON: Pin,
+	PSUSB: Pin,
+	SYSON: Pin,
+{
+	fn reset_ticks(&mut self, ticks: usize) {
+		self.reset_pin.set_high();
+		for _ in 0..ticks {
+			unsafe {
+				::core::arch::asm!("NOP");
+			}
+		}
+		self.reset_pin.set_low();
+	}
+
+	fn power_ticks(&mut self, ticks: usize) {
+		self.power_pin.set_high();
+		for _ in 0..ticks {
+			unsafe {
+				::core::arch::asm!("NOP");
+			}
+		}
+		self.power_pin.set_low();
+	}
+
+	fn current_state(&self) -> super::PowerState {
+		self.current_state
+	}
+
+	fn power_requested(&self) -> bool {
+		self.sys_on_pin.is_low()
+	}
+
+	unsafe fn set_power_state(&mut self, new_state: super::PowerState) {
+		match new_state {
+			super::PowerState::Off => {
+				self.psu_on_pin.set_low();
+				self.psu_standby_pin.set_low();
+			}
+			super::PowerState::Standby => {
+				self.psu_on_pin.set_low();
+				self.psu_standby_pin.set_high();
+			}
+			super::PowerState::On => {
+				self.psu_on_pin.set_high();
+				self.psu_standby_pin.set_high();
+			}
+		}
+
+		self.current_state = new_state;
 	}
 }
--- Firmware/src/uc/stm32/stm32f479vg.rs
+++ Firmware/src/uc/stm32/stm32f479vg.rs
@@ -39,8 +39,8 @@ pub fn init() -> (
 	let indicators = crate::chip::is31fl3218::Is31fl3218::new(i2c);
 
 	(
-		peripheral::DebugLed::new(Output::new(p.PE12, Level::Low, Speed::Low)),
-		peripheral::SystemUnderTest::new(
+		super::DebugLed::new(Output::new(p.PE12, Level::Low, Speed::Low)),
+		super::SystemUnderTest::new(
 			Output::new(p.PC9, Level::Low, Speed::Low),
 			Output::new(p.PC8, Level::Low, Speed::Low),
 			Output::new(p.PD6, Level::Low, Speed::Low),
@@ -49,124 +49,4 @@ pub fn init() -> (
 		),
 		super::Is31fl3218IndicatorLights::<_, 0, 1, 17, 12, 13, 11, 16, 14, 15>::new(indicators),
 	)
-}
-
-pub mod peripheral {
-	use crate::uc;
-	use embassy_stm32::gpio::{Input, Output, Pin};
-
-	pub struct DebugLed<'d, P: Pin> {
-		pin: Output<'d, P>,
-	}
-
-	impl<'d, P: Pin> DebugLed<'d, P> {
-		pub fn new(pin: Output<'d, P>) -> Self {
-			Self { pin }
-		}
-	}
-
-	impl<P: Pin> uc::DebugLed for DebugLed<'_, P> {
-		fn set_bit(&mut self, on: bool) {
-			self.pin.set_level(on.into());
-		}
-	}
-
-	pub struct SystemUnderTest<'d, RST, PWR, PSUON, PSUSB, SYSON>
-	where
-		RST: Pin,
-		PWR: Pin,
-		PSUON: Pin,
-		PSUSB: Pin,
-		SYSON: Pin,
-	{
-		current_state: uc::PowerState,
-		reset_pin: Output<'d, RST>,
-		power_pin: Output<'d, PWR>,
-		psu_on_pin: Output<'d, PSUON>,
-		psu_standby_pin: Output<'d, PSUSB>,
-		sys_on_pin: Input<'d, SYSON>,
-	}
-
-	impl<'d, RST, PWR, PSUON, PSUSB, SYSON> SystemUnderTest<'d, RST, PWR, PSUON, PSUSB, SYSON>
-	where
-		RST: Pin,
-		PWR: Pin,
-		PSUON: Pin,
-		PSUSB: Pin,
-		SYSON: Pin,
-	{
-		pub fn new(
-			reset_pin: Output<'d, RST>,
-			power_pin: Output<'d, PWR>,
-			psu_on_pin: Output<'d, PSUON>,
-			psu_standby_pin: Output<'d, PSUSB>,
-			sys_on_pin: Input<'d, SYSON>,
-		) -> Self {
-			Self {
-				current_state: uc::PowerState::Off,
-				reset_pin,
-				power_pin,
-				psu_on_pin,
-				psu_standby_pin,
-				sys_on_pin,
-			}
-		}
-	}
-
-	impl<'d, RST, PWR, PSUON, PSUSB, SYSON> uc::SystemUnderTest
-		for SystemUnderTest<'d, RST, PWR, PSUON, PSUSB, SYSON>
-	where
-		RST: Pin,
-		PWR: Pin,
-		PSUON: Pin,
-		PSUSB: Pin,
-		SYSON: Pin,
-	{
-		fn reset_ticks(&mut self, ticks: usize) {
-			self.reset_pin.set_high();
-			for _ in 0..ticks {
-				unsafe {
-					::core::arch::asm!("NOP");
-				}
-			}
-			self.reset_pin.set_low();
-		}
-
-		fn power_ticks(&mut self, ticks: usize) {
-			self.power_pin.set_high();
-			for _ in 0..ticks {
-				unsafe {
-					::core::arch::asm!("NOP");
-				}
-			}
-			self.power_pin.set_low();
-		}
-
-		fn current_state(&self) -> uc::PowerState {
-			self.current_state
-		}
-
-		fn power_requested(&self) -> bool {
-			self.sys_on_pin.is_low()
-		}
-
-		unsafe fn set_power_state(&mut self, new_state: uc::PowerState) {
-			match new_state {
-				uc::PowerState::Off => {
-					self.psu_on_pin.set_low();
-					self.psu_standby_pin.set_low();
-				}
-				uc::PowerState::Standby => {
-					self.psu_on_pin.set_low();
-					self.psu_standby_pin.set_high();
-				}
-				uc::PowerState::On => {
-					self.psu_on_pin.set_high();
-					self.psu_standby_pin.set_high();
-				}
-			}
-
-			self.current_state = new_state;
-		}
-	}
 }
