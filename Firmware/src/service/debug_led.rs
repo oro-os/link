f926a8c118c78f97cf89bdda4872d1f36ccd9353
Josh Junon <junon@oro.sh>
2023-11-18 15:11:49 +0100

    make debug led blink a bit quicker

--- Firmware/src/service/debug_led.rs
+++ Firmware/src/service/debug_led.rs
@@ -4,7 +4,7 @@ use embassy_time::{Duration, Timer};
 pub async fn run<L: DebugLed>(mut debug_led: L) -> ! {
 	loop {
 		debug_led.on();
-		Timer::after(Duration::from_millis(100)).await;
+		Timer::after(Duration::from_millis(40)).await;
 		debug_led.off();
 		Timer::after(Duration::from_millis(2000)).await;
 	}
